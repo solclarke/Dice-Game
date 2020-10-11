2597bce38f2603c27f25a3b29e7b7e5f94dc2ce4
solclarke <69057860+solclarke@users.noreply.github.com>
2020-10-11 18:20:07 +0100

    Refactor

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -19,11 +19,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //If player 1 wins, if player 2 wins, if a draw
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  document.querySelector("h2").innerHTML = "Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h2").innerHTML = "Player 2 Wins!";
 }
 else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  document.querySelector("h2").innerHTML = "Draw!";
 }
